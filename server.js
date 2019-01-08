484d56004f7bbb42e6950c6c838684beb54d60ed
john2796 <jbmiranda22796@gmail.com>
2019-01-08 13:16:55 -0800

    add all before deploying

--- server.js
+++ server.js
@@ -36,6 +36,13 @@ app.use("/api/users", users);
 // commentbox
 app.use("/api", commentbox);
 //http://localhost:5000/api/comments
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 //const port = process.env.PORT || 5000;
 app.listen(5000, () => console.log(`server running on port 5000`));
