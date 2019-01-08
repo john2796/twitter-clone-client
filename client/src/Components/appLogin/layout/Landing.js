e0f5ed7400fbf60aee19728119e95ff24f048439
john2796 <jbmiranda22796@gmail.com>
2019-01-07 16:25:13 -0800

    added login style

--- client/src/Components/appLogin/layout/Landing.js
+++ client/src/Components/appLogin/layout/Landing.js
@@ -1,5 +1,11 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const Title = styled.h2`
+  @import url("https://fonts.googleapis.com/css?family=Charm");
+  font-size: 30px;
+`;
 
 class Landing extends Component {
   render() {
@@ -7,15 +13,11 @@ class Landing extends Component {
       <div style={{ height: "75vh" }} className="container valign-wrapper">
         <div className="row">
           <div className="col s12 center-align">
-            <h4>
-              <b>Build</b> a login/auth app with the{" "}
-              <span style={{ fontFamily: "monospace" }}>MERN</span> stack from
-              scratch
-            </h4>
-            <p className="flow-text grey-text text-darken-1">
-              Create a (minimal) full-stack app with user authentication via
-              passport and JWTs
-            </p>
+            <Title>Instagram</Title>
+            <img
+              src="https://ultimatemember.com/wp-content/uploads/bb-plugin/cache/instagram-circle.png"
+              alt="..."
+            />
             <br />
             <Link
               to="/register"
